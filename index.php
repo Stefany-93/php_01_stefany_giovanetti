a82abf1acdf725515284e1a37d8628f29e2817f3
Stefany Giovanetti <ste_giovanetti@yahoo.it>
2025-07-20 11:50:53 +0200

    corretta seconda parte

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-$integer = 6;
-$float = 2.3;
-$string = "Ciao";
-$booleano = true;
+const INTERO = 6;
+const VIRGOLA = 2.3;
+const STRINGA = "Ciao";
+const BOOLEANO = true;
 
-var_dump($integer);
-var_dump($float);
-var_dump($string);
-var_dump($booleano);
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
